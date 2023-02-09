5fc9eff22c0089f8d06ac68ecb1421efc3eca9b5
Sierra <sierra.r.jordan@gmail.com>
2023-02-09 15:35:02 -0800

    Initial Commit

---
+++ festipalsApp/festipalsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  festipalsAppApp.swift
+//  festipalsApp
+//
+//  Created by sierra jordan on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct festipalsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
