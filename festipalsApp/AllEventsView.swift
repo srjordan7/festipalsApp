7d054f58808696bf2b882ae51e45529fcd227d20
Sierra <sierra.r.jordan@gmail.com>
2023-02-09 22:38:43 -0800

    adds loads user data on AllEventsView

--- festipalsApp/AllEventsView.swift
+++ festipalsApp/AllEventsView.swift
@@ -6,9 +6,53 @@
 //
 
 import SwiftUI
+import Firebase
+
+struct User {
+    let uid, email, profileImageUrl, name: String
+}
+
+class AllEventsViewModel: ObservableObject {
+    
+    @Published var errorMsg = ""
+    @Published var user: User?
+    
+    init() {
+        fetchCurrentUser()
+    }
+    
+    // get current user data
+    private func fetchCurrentUser() {
+        guard let uid = Auth.auth().currentUser?.uid else {
+            self.errorMsg = "could not find user data"
+            return }
+        
+        Firestore.firestore().collection("users").document(uid).getDocument { snapshot, error in
+            if let error = error {
+                self.errorMsg = "failed to fetch current user: \(error.localizedDescription)"
+                print("failed to fetch current user: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let data = snapshot?.data() else {
+                self.errorMsg = "no data found"
+                return }
+            
+            // build User from data
+            let uid = data["uid"] as? String ?? ""
+            let email = data["email"] as? String ?? ""
+            let profileImageUrl = data["profileImageUrl"] as? String ?? ""
+            let name = data["name"] as? String ?? ""
+            self.user = User(uid: uid, email: email, profileImageUrl: profileImageUrl, name: name)
+        }
+    }
+    
+}
 
 struct AllEventsView: View {
     @State var showLogOutOptions = false
+    
+    @ObservedObject private var vm = AllEventsViewModel()
     
     var body: some View {
         NavigationView {
@@ -17,7 +61,17 @@ struct AllEventsView: View {
                 VStack {
                     HStack {
                         // current account picture
-                        Image(systemName: "person")
+                        AsyncImage(url: URL(string: vm.user?.profileImageUrl ?? "")) { returnedImage in
+                            returnedImage
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 50, height: 50)
+                                .clipped()
+                                .cornerRadius(50)
+                        } placeholder: {
+                            Image(systemName: "person")
+                        }
+
                         Spacer()
                         // log out button
                         Button {
@@ -37,7 +91,7 @@ struct AllEventsView: View {
                         ])
                     }
                     
-                    Text("hi, name!")
+                    Text("hi, \(vm.user?.name ?? "")!")
                     Text("upcoming events")
                         .font(.system(size: 26))
                 }
@@ -80,6 +134,8 @@ struct AllEventsView: View {
                     .shadow(radius: 5)
                 }, alignment: .bottom)
             .navigationBarHidden(true)
+            .background(Color("BackgroundColor")
+                .ignoresSafeArea()) // background color
         }
     }
 }
